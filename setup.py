725edde8b8efec44226bb77376c880a429aca57d
Ashish Kaushal <kaushal.ashish@dozee.io>
2022-06-18 11:54:21 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import  setup
+from setuptools import  setup,find_packages
 from typing import List
 
 
@@ -19,7 +19,7 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -27,7 +27,7 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 )
 
